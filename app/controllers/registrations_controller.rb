47ed486a801ab36437cb160c93905d0b51830d25
solivaresr <kolivaresrojas@gmail.com>
2022-06-27 14:12:21 -0400

    update: manejando errores

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,11 +5,17 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: 'Cuenta creada!'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit()
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
