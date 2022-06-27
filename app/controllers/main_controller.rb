c588613944e12251e189dccce60d8312cb308c03
solivaresr <kolivaresrojas@gmail.com>
2022-06-27 14:47:22 -0400

    update: session cookie log in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in successfully"
+    #flash[:notice] = "Logged in successfully"
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
