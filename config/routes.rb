8ea31c2e79ed2f34bf35b942dcad5b822ac2add7
solivaresr <kolivaresrojas@gmail.com>
2022-07-02 17:49:45 -0400

    login system

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post 'sign_up', to: 'registrations#create'
   delete 'logout', to: 'sessions#destroy'
 
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  
   # Defines the root path route ("/")
   root "main#index"
 end
