21fcb1747c81f0f7c7305f31f378fd8db3855c9d
solivaresr <kolivaresrojas@gmail.com>
2022-07-09 15:44:17 -0400

    Agrego uso se Current.user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,4 @@
 class MainController < ApplicationController
   def index
-    #flash[:notice] = "Logged in successfully"
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
   end
 end
